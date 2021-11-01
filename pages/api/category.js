04827109bdad1c68639a1a3fa58ed669f10625fc
Blake Parkinson <wizardplow@gmail.com>
2021-11-01 13:07:25 -0600

    dont hit ditto

--- pages/api/category.js
+++ pages/api/category.js
@@ -1,6 +1,25 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://dittofi.com/409/iapi/v1/GetCategories")
-  const json = await response.json()
+  // const response = await fetch("https://dittofi.com/409/iapi/v1/GetCategories")
+  const json = {
+    data: [
+      { category: "" },
+      { category: "AfterSchool-Academics" },
+      { category: "AfterSchool-Athletics" },
+      { category: "AfterSchool-TheArts" },
+      { category: "Auto Sales/Repairs" },
+      { category: "Dentist/Orthodontist" },
+      { category: "Family Fun" },
+      { category: "Kids/Teen Retail" },
+      { category: "Lawyers" },
+      { category: "Nonprofit" },
+      { category: "Realtors" },
+      { category: "Software" },
+      { category: "Virtual Camp" },
+      { category: "k-12 Public School" },
+    ],
+    message: "success",
+    error: "",
+  }
   json.data.forEach((categoryItem) => {
     if (categoryItem.category == "") {
       categoryItem.label = "All Categories"
