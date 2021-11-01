230dfd42e5c353e1bbfefb0ccc71fe23c0124563
Blake Parkinson <wizardplow@gmail.com>
2021-11-01 13:13:17 -0600

    another update

--- pages/api/category.js
+++ pages/api/category.js
@@ -2,30 +2,23 @@ export default async function handler(req, res) {
   // const response = await fetch("https://dittofi.com/409/iapi/v1/GetCategories")
   const json = {
     data: [
-      { category: "" },
-      { category: "AfterSchool-Academics" },
-      { category: "AfterSchool-Athletics" },
-      { category: "AfterSchool-TheArts" },
-      { category: "Auto Sales/Repairs" },
-      { category: "Dentist/Orthodontist" },
-      { category: "Family Fun" },
-      { category: "Kids/Teen Retail" },
-      { category: "Lawyers" },
-      { category: "Nonprofit" },
-      { category: "Realtors" },
-      { category: "Software" },
-      { category: "Virtual Camp" },
-      { category: "k-12 Public School" },
+      { category: "", label: "All Categories" },
+      { category: "AfterSchool-Academics", label: "After School-Academics" },
+      { category: "AfterSchool-Athletics", label: "After School-Athletics" },
+      { category: "AfterSchool-TheArts", label: "After School-TheArts" },
+      { category: "Auto Sales/Repairs", label: "Auto Sales/Repairs" },
+      { category: "Dentist/Orthodontist", label: "Dentist/Orthodontist" },
+      { category: "Family Fun", label: "Family Fun" },
+      { category: "Kids/Teen Retail", label: "Kids/Teen Retail" },
+      { category: "Lawyers", label: "Lawyers" },
+      { category: "Nonprofit", label: "Nonprofit" },
+      { category: "Realtors", label: "Realtors" },
+      { category: "Software", label: "Software" },
+      { category: "Virtual Camp", label: "Virtual Camp" },
+      { category: "k-12 Public School", label: "K-12 Public School" },
     ],
     message: "success",
     error: "",
   }
-  json.data.forEach((categoryItem) => {
-    if (categoryItem.category == "") {
-      categoryItem.label = "All Categories"
-    } else {
-      categoryItem.label = categoryItem.category
-    }
-  })
   res.status(200).json(json)
 }
