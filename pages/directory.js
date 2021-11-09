2d0e0c6cae187411f9a1071e50402eaa68c4235e
Blake Parkinson <wizardplow@gmail.com>
2021-11-08 20:41:10 -0700

    adds the button

--- pages/directory.js
+++ pages/directory.js
@@ -12,6 +12,8 @@ import Grid from "@mui/material/Grid"
 // import Avatar from '@mui/material/Avatar'
 import Typography from "@mui/material/Typography"
 import InputBase from "@mui/material/InputBase"
+import Button from "@mui/material/Button"
+
 import Card from "@mui/material/Card"
 import CardActions from "@mui/material/CardActions"
 import CardContent from "@mui/material/CardContent"
@@ -359,7 +361,16 @@ function DirectoryPage({ availableCategories }) {
                           : DEFAULT_DESC}
                       </Typography>
                     </CardContent>
-                    <CardActions></CardActions>
+                    <CardActions>
+                      <a
+                        className="w-full"
+                        href={`https://community.digitaldeets.com/home?communityId=${listing.id}`}
+                      >
+                        <Button className="w-full self-end bg-orange text-white">
+                          Signup up my organization
+                        </Button>
+                      </a>
+                    </CardActions>
                   </Card>
                 </Grid>
               )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "#ee6c34",
+        blue: "#02779d",
+      },
+    },
   },
   variants: {
     extend: {},
