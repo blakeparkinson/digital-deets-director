0f1de6aca36f1fb823f1ee673564ef27613e9534
Blake Parkinson <wizardplow@gmail.com>
2021-12-19 12:05:58 -0700

    more debugging

--- pages/api/category.js
+++ pages/api/category.js
@@ -3,5 +3,6 @@ export default async function handler(req, res) {
     'https://app.digitaldeets.com/api_catalog/categories'
   )
   const data = await response.json()
+  console.log('dc: ', data.categories)
   res.status(200).json({ data: data.categories })
 }
