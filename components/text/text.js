bf635b76a059002ea7fe22ed8e4140ec312dd34e
Blake Parkinson <wizardplow@gmail.com>
2022-02-05 14:48:58 -0700

    Revert "Adding swc"
    This reverts commit 4c2e2ac7c937ace014c61fcf688e4f52b3ed2fcf.

--- components/text/text.js
+++ components/text/text.js
@@ -1,41 +1,42 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
+import { theme } from "twin.macro"
 
 const levelToElementMap = {
-  xxxs: 'h6',
-  xxs: 'h5',
-  xs: 'h4',
-  s: 'h3',
-  m: 'h2',
-  lg: 'h2',
+  xxxs: "h6",
+  xxs: "h5",
+  xs: "h4",
+  s: "h3",
+  m: "h2",
+  lg: "h2",
 }
 
 const levelToFontSizeMap = {
-  xxxs: '8',
-  xxs: '10',
-  xs: '12',
-  s: '14',
-  m: '16',
-  lg: '18',
+  xxxs: "8",
+  xxs: "10",
+  xs: "12",
+  s: "14",
+  m: "16",
+  lg: "18",
 }
 
 const levelToLineHeightMap = {
-  xxxs: '150%',
-  xxs: '150%',
-  xs: ['150%', '13px'],
-  s: '150%',
-  m: '22px',
-  lg: '150%',
+  xxxs: "150%",
+  xxs: "150%",
+  xs: ["150%", "13px"],
+  s: "150%",
+  m: "22px",
+  lg: "150%",
 }
 
 const weightToFontWeightMap = {
-  normal: 'normal',
-  medium: '500',
-  bold: '700',
+  normal: "normal",
+  medium: "500",
+  bold: "700",
 }
 
 const TextNode = styled.h2`
-  ${({ level = 's', weight = 'medium', color, heading, lineHeight }) => {
+  ${({ level = "s", weight = "medium", color, heading, lineHeight }) => {
     if (!lineHeight) {
       lineHeight = levelToLineHeightMap[level]
     }
@@ -60,12 +61,12 @@ export const Text = ({
   style,
   weight,
   children,
-  level = 'm',
+  level = "m",
   heading = false,
-  color = '#000',
+  color = "#000",
   ...rest
 }) => {
-  const as = heading ? levelToElementMap[level] : 'p'
+  const as = heading ? levelToElementMap[level] : "p"
 
   return (
     <TextNode
