d3a0091c75dabc214d85155a09cce6d004380307
Blake Parkinson <wizardplow@gmail.com>
2022-02-05 14:59:20 -0700

    Revert "Revert "Revert "Adding swc"""
    This reverts commit 350c6f70a361485eef396542f7d54e66d37de3b1.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false }
-
-    return config
-  },
 }
