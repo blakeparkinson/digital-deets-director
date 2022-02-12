f484683e8516d64e0807dc05a7489fd056a2fd6d
Blake Parkinson <wizardplow@gmail.com>
2022-02-12 10:20:06 -0700

    cleanup

--- pages/api/category.js
+++ pages/api/category.js
@@ -1,9 +1,7 @@
 export default async function handler(req, res) {
-  console.log('here ehere ')
   const response = await fetch(
     'https://app.digitaldeets.com/api_catalog/categories'
   )
   const data = await response.json()
-  console.log('dc: ', data.categories)
   res.status(200).json({ data: data.categories })
 }
